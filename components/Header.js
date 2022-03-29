9465672a39ef6a232bc3d721d4ef30aab096c43b
Jose Farias <josevictorjop@gmail.com>
2022-03-28 23:05:30 -0300

    First

---
+++ components/Header.js
@@ -0,0 +1,14 @@
+import headerStyles from '../styles/Header.module.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className={headerStyles.title}>
+                <span>WebDev</span> News
+            </h1>
+            <p className={headerStyles.description}> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, eligendi?</p>
+        </div>
+    );
+}
+
+export default Header;
